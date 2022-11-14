ff94bf90043daa2bae449dd9e6ca81f107536c5d
MJ-Curtido <manu17pc@gmail.com>
2022-11-14 12:49:20 +0100

    V1-Cliente hay que hacerlo

--- src/main/java/com/mycompany/hlc/ej2_servidoryclienteweb/Cliente.java
+++ src/main/java/com/mycompany/hlc/ej2_servidoryclienteweb/Cliente.java
@@ -4,10 +4,9 @@
  */
 package com.mycompany.hlc.ej2_servidoryclienteweb;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +14,7 @@ import java.net.Socket;
  */
 public class Cliente {
     public static void main(String[] args) throws IOException {
+        Scanner leer = new Scanner(System.in);
         String host = "localhost";
         int puertoRemoto = 6007;
 
@@ -22,6 +22,9 @@ public class Cliente {
 
         //hacer que se pida una url con un scanner y que esa url la separe por barras y puntos con split que te lo separa en arrays
 
+        System.out.println("Introduzca una url");
+        String url = leer.next();
+        
         cliente.close();
     }
 }
